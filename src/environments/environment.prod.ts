af45ac0a989b42acd2b7df131df1190672b1187a
rafysanchez <rafysanchez@hotmail.com>
2018-11-21 17:02:34 -0200

    adjs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDFA-FkqzZldDUM5PXTK4F6qI6mc7Q7H-s",
+    authDomain: "angular6-crud-dd28f.firebaseapp.com",
+    databaseURL: "https://angular6-crud-dd28f.firebaseio.com",
+    projectId: "angular6-crud-dd28f",
+    storageBucket: "angular6-crud-dd28f.appspot.com",
+    messagingSenderId: "719465988254"
+  }
 };
